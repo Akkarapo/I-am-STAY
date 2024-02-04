95bf5d6564ff0e3ab09cff15255a6737e656935e
Akkarapong Garpaya <112610820+Akkarapo@users.noreply.github.com>
2024-02-04 14:09:10 +0700

    Pak_reserve

---
+++ Pak_reserve1/Pak_reserve1/PakForm.cpp
@@ -0,0 +1,13 @@
+#include "PakForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+
+void main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Pakreserve1::PakForm form;
+    Application::Run(% form);
+
+}
---
+++ Pak_reserve1/Pak_reserve1/PakForm.h
@@ -0,0 +1,80 @@
+#pragma once
+
+namespace Pakreserve1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for PakForm
+	/// </summary>
+	public ref class PakForm : public System::Windows::Forms::Form
+	{
+	public:
+		PakForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~PakForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(PakForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(130, 47);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(44, 16);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"label1";
+			// 
+			// PakForm
+			// 
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(1252, 673);
+			this->Controls->Add(this->label1);
+			this->Name = L"PakForm";
+			this->Text = L"PakForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
