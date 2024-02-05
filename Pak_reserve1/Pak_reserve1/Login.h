2684c9f1be56887ad8ba9d8dcc4e33c1ee98fac5
GNOGz <99389733+GNOGz@users.noreply.github.com>
2024-02-06 00:55:28 +0700

    add input field

--- Pak_reserve1/Pak_reserve1/Login.h
+++ Pak_reserve1/Pak_reserve1/Login.h
@@ -34,6 +34,12 @@ namespace Pakreserve1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,18 +54,73 @@ namespace Pakreserve1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(747, 260);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(70, 16);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Username";
+			this->label1->Click += gcnew System::EventHandler(this, &Login::label1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(750, 294);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(449, 22);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(750, 385);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(449, 22);
+			this->textBox2->TabIndex = 2;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(750, 346);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(67, 16);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Password";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(753, 462);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(446, 46);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Sign In";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// Login
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1252, 673);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
 			this->Name = L"Login";
 			this->Text = L"Login";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
