3b167fd0b26747ab1e434ca95c419080cc62a11b
Akkarapong Garpaya <112610820+Akkarapo@users.noreply.github.com>
2024-02-06 22:02:13 +0700

    loginbyP

--- Pak_reserve1/Login.h
+++ Pak_reserve1/Login.h
@@ -38,7 +38,7 @@ namespace Pakreserve1 {
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Button^ button1;
+
 
 
 
@@ -46,6 +46,10 @@ namespace Pakreserve1 {
 
 	private: System::Windows::Forms::Label^ label8;
 	private: System::Windows::Forms::Button^ button2;
+
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Button^ button1;
+
 
 
 	protected:
@@ -68,74 +72,107 @@ namespace Pakreserve1 {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label8 = (gcnew System::Windows::Forms::Label());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(654, 276);
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei Light", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(703, 268);
+			this->label1->Margin = System::Windows::Forms::Padding(0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(67, 17);
+			this->label1->Size = System::Drawing::Size(104, 27);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Username";
 			this->label1->Click += gcnew System::EventHandler(this, &Login::label1_Click);
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(656, 312);
+			this->textBox1->AllowDrop = true;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 16));
+			this->textBox1->Location = System::Drawing::Point(745, 305);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(393, 25);
+			this->textBox1->Size = System::Drawing::Size(430, 36);
 			this->textBox1->TabIndex = 1;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(656, 409);
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 14));
+			this->textBox2->Location = System::Drawing::Point(745, 385);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(393, 25);
+			this->textBox2->Size = System::Drawing::Size(430, 39);
 			this->textBox2->TabIndex = 2;
 			this->textBox2->UseSystemPasswordChar = true;
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(656, 368);
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei Light", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(702, 353);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(64, 17);
+			this->label2->Size = System::Drawing::Size(97, 27);
 			this->label2->TabIndex = 3;
 			this->label2->Text = L"Password";
 			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(659, 491);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(390, 49);
-			this->button1->TabIndex = 4;
-			this->button1->Text = L"Sign In";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Login::button1_Click);
-			// 
 			// label8
 			// 
+			this->label8->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
 			this->label8->AutoSize = true;
-			this->label8->Font = (gcnew System::Drawing::Font(L"Segoe UI", 20));
-			this->label8->Location = System::Drawing::Point(783, 67);
+			this->label8->BackColor = System::Drawing::Color::Transparent;
+			this->label8->Font = (gcnew System::Drawing::Font(L"Bell MT", 19.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label8->Location = System::Drawing::Point(904, 75);
 			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(112, 46);
+			this->label8->Size = System::Drawing::Size(112, 39);
 			this->label8->TabIndex = 10;
 			this->label8->Text = L"Log in";
+			this->label8->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(659, 562);
+			this->button2->Location = System::Drawing::Point(852, 598);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(390, 49);
+			this->button2->Size = System::Drawing::Size(220, 49);
 			this->button2->TabIndex = 11;
 			this->button2->Text = L"Exit";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &Login::button2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei Light", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(758, 120);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(359, 19);
+			this->label3->TabIndex = 13;
+			this->label3->Text = L"Enter your username and password to access your account";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// button1
+			// 
+			this->button1->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
+			this->button1->BackColor = System::Drawing::Color::Transparent;
+			this->button1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.BackgroundImage")));
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->button1->Location = System::Drawing::Point(741, 460);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(438, 56);
+			this->button1->TabIndex = 14;
+			this->button1->UseVisualStyleBackColor = false;
 			// 
 			// Login
 			// 
@@ -144,9 +181,10 @@ namespace Pakreserve1 {
 			this->BackColor = System::Drawing::Color::WhiteSmoke;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->ClientSize = System::Drawing::Size(1262, 673);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label8);
-			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
