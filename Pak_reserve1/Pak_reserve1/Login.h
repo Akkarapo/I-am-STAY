fdf2fd35b4242d578177f4958f6ea802b133906c
Akkarapong Garpaya <112610820+Akkarapo@users.noreply.github.com>
2024-02-06 15:34:01 +0700

    TestLogIn

--- Pak_reserve1/Pak_reserve1/Login.h
+++ Pak_reserve1/Pak_reserve1/Login.h
@@ -61,6 +61,7 @@ namespace Pakreserve1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Login::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
@@ -78,7 +79,8 @@ namespace Pakreserve1 {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(654, 276);
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Location = System::Drawing::Point(740, 218);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(67, 17);
 			this->label1->TabIndex = 0;
@@ -87,14 +89,14 @@ namespace Pakreserve1 {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(656, 312);
+			this->textBox1->Location = System::Drawing::Point(755, 250);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(393, 25);
 			this->textBox1->TabIndex = 1;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(656, 409);
+			this->textBox2->Location = System::Drawing::Point(755, 409);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(393, 25);
 			this->textBox2->TabIndex = 2;
@@ -103,7 +105,8 @@ namespace Pakreserve1 {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(656, 368);
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Location = System::Drawing::Point(740, 368);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(64, 17);
 			this->label2->TabIndex = 3;
@@ -111,7 +114,7 @@ namespace Pakreserve1 {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(659, 491);
+			this->button1->Location = System::Drawing::Point(755, 491);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(390, 49);
 			this->button1->TabIndex = 4;
@@ -122,57 +125,70 @@ namespace Pakreserve1 {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(10, 22);
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->ForeColor = System::Drawing::Color::White;
+			this->label3->Location = System::Drawing::Point(16, 22);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(78, 17);
+			this->label3->Size = System::Drawing::Size(98, 17);
 			this->label3->TabIndex = 5;
-			this->label3->Text = L"?????????? 1";
+			this->label3->Text = L"I STAY You STAY";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::Color::Transparent;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 30));
-			this->label4->Location = System::Drawing::Point(10, 400);
+			this->label4->ForeColor = System::Drawing::Color::White;
+			this->label4->Location = System::Drawing::Point(16, 400);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(437, 58);
+			this->label4->Size = System::Drawing::Size(200, 58);
 			this->label4->TabIndex = 6;
-			this->label4->Text = L"??????????????????";
+			this->label4->Text = L"Simplify";
+			this->label4->Click += gcnew System::EventHandler(this, &Login::label4_Click);
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
+			this->label5->BackColor = System::Drawing::Color::Transparent;
 			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 30));
-			this->label5->Location = System::Drawing::Point(10, 471);
+			this->label5->ForeColor = System::Drawing::Color::White;
+			this->label5->Location = System::Drawing::Point(16, 465);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(298, 58);
+			this->label5->Size = System::Drawing::Size(437, 58);
 			this->label5->TabIndex = 7;
-			this->label5->Text = L"??????????2";
+			this->label5->Text = L"Your Reservations";
 			// 
 			// label6
 			// 
 			this->label6->AutoSize = true;
+			this->label6->BackColor = System::Drawing::Color::Transparent;
 			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 30));
-			this->label6->Location = System::Drawing::Point(10, 545);
+			this->label6->ForeColor = System::Drawing::Color::Transparent;
+			this->label6->Location = System::Drawing::Point(16, 540);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(183, 58);
+			this->label6->Size = System::Drawing::Size(276, 58);
 			this->label6->TabIndex = 8;
-			this->label6->Text = L"??????";
+			this->label6->Text = L"Experience";
 			// 
 			// label7
 			// 
 			this->label7->AutoSize = true;
+			this->label7->BackColor = System::Drawing::Color::Transparent;
 			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9));
-			this->label7->Location = System::Drawing::Point(17, 657);
+			this->label7->ForeColor = System::Drawing::Color::White;
+			this->label7->Location = System::Drawing::Point(16, 640);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(160, 18);
+			this->label7->Size = System::Drawing::Size(372, 18);
 			this->label7->TabIndex = 9;
-			this->label7->Text = L"???????????????????????";
+			this->label7->Text = L"Sociable Seating Reservations Everywhere Made Easy.";
+			this->label7->Click += gcnew System::EventHandler(this, &Login::label7_Click);
 			// 
 			// label8
 			// 
 			this->label8->AutoSize = true;
+			this->label8->BackColor = System::Drawing::Color::Transparent;
 			this->label8->Font = (gcnew System::Drawing::Font(L"Segoe UI", 20));
-			this->label8->Location = System::Drawing::Point(783, 67);
+			this->label8->Location = System::Drawing::Point(905, 59);
 			this->label8->Name = L"label8";
 			this->label8->Size = System::Drawing::Size(112, 46);
 			this->label8->TabIndex = 10;
@@ -180,7 +196,7 @@ namespace Pakreserve1 {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(659, 562);
+			this->button2->Location = System::Drawing::Point(755, 562);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(390, 49);
 			this->button2->TabIndex = 11;
@@ -192,7 +208,8 @@ namespace Pakreserve1 {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(7, 17);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1096, 715);
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(1262, 673);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label8);
 			this->Controls->Add(this->label7);
@@ -207,8 +224,10 @@ namespace Pakreserve1 {
 			this->Controls->Add(this->label1);
 			this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"Login";
 			this->Text = L"Login";
+			this->Load += gcnew System::EventHandler(this, &Login::Login_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -220,6 +239,12 @@ namespace Pakreserve1 {
 	}
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 	Application::Exit();
+}
+private: System::Void Login_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label7_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
