184b870e8663dd14d17326cebdccbae2e6d2d50c
GNOGz <99389733+GNOGz@users.noreply.github.com>
2024-02-27 23:36:48 +0700

    update nid noi

--- Pak_reserve1/MPBarnd.h
+++ Pak_reserve1/MPBarnd.h
@@ -51,8 +51,8 @@ namespace Pakreserve1 {
 			fileIn.close();
 			ofstream fileOut(path, ios::app);
 			fileOut << datec << " " << timec << endl;
-			fileOut.close();*/
-		}
+			fileOut.close();
+		}	
 
 	protected:
 		/// <summary>
