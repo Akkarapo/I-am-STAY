e231c7b0a6155ad4dbe3256b9f17bf419be305eb
AmericanoHoney <cocoart033@gmail.com>
2024-03-04 00:13:35 +0700

    Add Table name

--- Pak_reserve1/PakForm.h
+++ Pak_reserve1/PakForm.h
@@ -2595,7 +2595,44 @@ private: System::Void ConfirmTableNo1_Click(System::Object^ sender, System::Even
 	fileOutUser << "BarName: " << "MaewNoi ";
 	for (int i = 0; i < dataTable->Length; i++)
 	{
-		if (dataTable[i]) fileOutUser << to_string(i) << " ";
+		if (dataTable[i]) //fileOutUser << to_string(i) << " ";
+		{
+			switch (i)
+			{
+			case 0: fileOutUser << "A1Table2P" << " "; break;
+			case 1: fileOutUser << "A2Table2P" << " "; break;
+			case 2: fileOutUser << "A3Table2P" << " "; break;
+			case 3: fileOutUser << "B1Table2P" << " "; break;
+			case 4: fileOutUser << "B2Table4P" << " "; break;
+			case 5: fileOutUser << "B3Table4P" << " "; break;
+			case 6: fileOutUser << "B4Table4P" << " "; break;
+			case 7: fileOutUser << "C1Table2P" << " "; break;
+			case 8: fileOutUser << "C2Table2P" << " "; break;
+			case 9: fileOutUser << "C3Table2P" << " "; break;
+			case 10: fileOutUser << "D1Table4P" << " "; break;
+			case 11: fileOutUser << "D2Table4P" << " "; break;
+			case 12: fileOutUser << "D3Table4P" << " "; break;
+			case 13: fileOutUser << "E1Table2P" << " "; break;
+			case 14: fileOutUser << "E2Table2P" << " "; break;
+			case 15: fileOutUser << "E3Table2P" << " "; break;
+			case 16: fileOutUser << "F1Table4P" << " "; break;
+			case 17: fileOutUser << "F2Table4P" << " "; break;
+			case 18: fileOutUser << "F3Table4P" << " "; break;
+			case 19: fileOutUser << "G1Table2P" << " "; break;
+			case 20: fileOutUser << "G2Table2P" << " "; break;
+			case 21: fileOutUser << "G3Table2P" << " "; break;
+			case 22: fileOutUser << "H1Table1P" << " "; break;
+			case 23: fileOutUser << "H2Table1P" << " "; break;
+			case 24: fileOutUser << "H3Table1P" << " "; break;
+			case 25: fileOutUser << "H4Table1P" << " "; break;
+			case 26: fileOutUser << "H5Table1P" << " "; break;
+			case 27: fileOutUser << "H6Table1P" << " "; break;
+			case 28: fileOutUser << "H7Table1P" << " "; break;
+			default:
+				break;
+			}
+		} 
+
 	}
 	fileOutUser << "Date: " << datec << " Time: " << timec << " complete" << endl;
 	fileOutUser.close();
