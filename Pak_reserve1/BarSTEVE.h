3354c48244d361d7b132189cefea9d8c0c4081d0
Akkarapong Garpaya <112610820+Akkarapo@users.noreply.github.com>
2024-03-04 03:57:29 +0700

    BarSTEVE

---
+++ Pak_reserve1/BarSTEVE.h
@@ -0,0 +1,69 @@
+#pragma once
+
+namespace Pakreserve1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for BarSTEVE
+	/// </summary>
+	public ref class BarSTEVE : public System::Windows::Forms::Form
+	{
+	public:
+		BarSTEVE(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~BarSTEVE()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(BarSTEVE::typeid));
+			this->SuspendLayout();
+			// 
+			// BarSTEVE
+			// 
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(1280, 720);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Name = L"BarSTEVE";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"BarSTEVE";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
