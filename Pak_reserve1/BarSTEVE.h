452041d3b7c64222a71a7147601fcb848e57e82b
AmericanoHoney <cocoart033@gmail.com>
2024-03-05 02:47:48 +0700

    Add People To Americano/Garpanya/Honey/Sajui/STEVE

--- Pak_reserve1/BarSTEVE.h
+++ Pak_reserve1/BarSTEVE.h
@@ -37,6 +37,7 @@ namespace Pakreserve1 {
 	public:
 		int tableSelect = 0;
 	private: System::Windows::Forms::PictureBox^ HomeBTN;
+	private: System::Windows::Forms::Label^ Table;
 	public:
 		int targetline = 12;
 		BarSTEVE(User^ user)
@@ -240,6 +241,7 @@ namespace Pakreserve1 {
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->ConfirmTableBarMapraw = (gcnew System::Windows::Forms::PictureBox());
 			this->HomeBTN = (gcnew System::Windows::Forms::PictureBox());
+			this->Table = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->A1Table1P))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->A2Table1P))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->A3Table1P))->BeginInit();
@@ -1209,12 +1211,25 @@ namespace Pakreserve1 {
 			this->HomeBTN->TabStop = false;
 			this->HomeBTN->Click += gcnew System::EventHandler(this, &BarSTEVE::HomeBTN_Click);
 			// 
+			// Table
+			// 
+			this->Table->BackColor = System::Drawing::Color::Transparent;
+			this->Table->Font = (gcnew System::Drawing::Font(L"Mongolian Baiti", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Table->ForeColor = System::Drawing::Color::White;
+			this->Table->Location = System::Drawing::Point(86, 494);
+			this->Table->Name = L"Table";
+			this->Table->Size = System::Drawing::Size(144, 40);
+			this->Table->TabIndex = 182;
+			this->Table->Text = L"XX/29";
+			// 
 			// BarSTEVE
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
 			this->ClientSize = System::Drawing::Size(1280, 720);
+			this->Controls->Add(this->Table);
 			this->Controls->Add(this->HomeBTN);
 			this->Controls->Add(this->ConfirmTableBarMapraw);
 			this->Controls->Add(this->panel1);
@@ -1968,6 +1983,22 @@ namespace Pakreserve1 {
 	}
 	private: System::Void BarSTEVE_Load(System::Object^ sender, System::EventArgs^ e) {
 		dataTable = gcnew array<bool>(29);
+		using namespace std;
+		String^ temp = Application::StartupPath + "\\Data\\" + "Table.txt";
+		string path, line;
+		int count = 0;
+		MarshalString(temp, path);
+		std::ifstream fileIn(path);
+		for (int i = 0; i < targetline; i++)
+		{
+			getline(fileIn, line);
+		}
+		count = 0;
+		for (int i = 0; i < line.size(); i++)
+		{
+			if (line[i] == '1') count++;
+		}
+		Table->Text = count.ToString() + "/" + line.size();
 	}
 	private: System::Void B6Table6P_Click_1(System::Object^ sender, System::EventArgs^ e) {
 		B6Table6P->Hide();
